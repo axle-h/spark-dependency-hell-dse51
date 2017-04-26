b05df65eb99d665619238edf103ebbcd4d1841a8
Alex Haslehurst <alex.haslehurst@utilitywise.com>
2017-04-26 14:59:28 +0100

    Added dirty joda-time

--- src/main/scala/axh/SparkDependencyHell.scala
+++ src/main/scala/axh/SparkDependencyHell.scala
@@ -2,27 +2,32 @@ package axh
 
 import org.apache.spark.{SparkConf, SparkContext}
 import org.slf4j.LoggerFactory
+import org.apache.avro.specific.SpecificDatumReader
+import org.joda.time.DateTime
+import scala.reflect._
 
 /**
   * Created by Alex.Haslehurst on 26/04/2017.
   */
 object SparkDependencyHell extends App {
 
-  def getAvroVersionString: String ={
-    val cls = classOf[org.apache.avro.specific.SpecificDatumReader[_]]
+  def getVersionString[A: ClassTag]: String ={
+    val cls = classTag[A].runtimeClass
     val pkg = cls.getPackage
     val jar = cls.getProtectionDomain.getCodeSource.getLocation
     s"$pkg $jar"
   }
 
+  def getVersionStrings: (String, String) = (getVersionString[DateTime], getVersionString[SpecificDatumReader[_]])
+
   val conf = new SparkConf()
   val sc = new SparkContext(conf)
 
   val log = LoggerFactory.getLogger(SparkDependencyHell.getClass)
-  log.error(s"Driver Version: $getAvroVersionString")
+  log.error(s"Driver Version: $getVersionStrings")
 
   val rdd = sc.parallelize(List(1))
-  val executorVersion = rdd.map(_ => getAvroVersionString).collect.head
+  val executorVersion = rdd.map(_ => getVersionStrings).collect.head
 
   log.error(s"Executor Version: $executorVersion")
 

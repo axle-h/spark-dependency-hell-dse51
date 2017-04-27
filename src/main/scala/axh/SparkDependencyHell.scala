ce8ffef40386bb5c329d351bf9675b9a8c802678
Alex Haslehurst <alex.haslehurst@utilitywise.com>
2017-04-27 14:35:05 +0100

    Added dependencies to the shaded jar and added example of issue this will cause

--- src/main/scala/axh/SparkDependencyHell.scala
+++ src/main/scala/axh/SparkDependencyHell.scala
@@ -4,12 +4,35 @@ import org.apache.spark.{SparkConf, SparkContext}
 import org.slf4j.LoggerFactory
 import org.apache.avro.specific.SpecificDatumReader
 import org.joda.time.DateTime
+
 import scala.reflect._
 
 /**
   * Created by Alex.Haslehurst on 26/04/2017.
   */
 object SparkDependencyHell extends App {
+
+  val conf = new SparkConf()
+  val sc = new SparkContext(conf)
+
+  def tryRunOnExecutor[A: ClassTag](name: String)(f: Unit => A): Unit ={
+    val log = LoggerFactory.getLogger(SparkDependencyHell.getClass)
+
+    // Run on driver.
+    log.error(s"[Driver] $name: ${f()}")
+
+    // Run on executor and collect result to log on driver.
+    sc.parallelize(List(1)).map { _ =>
+      try {
+        f()
+      } catch {
+        case e: Throwable => e
+      }
+    }.collect.head match {
+      case e: Throwable => log.error(s"[Executor] Failed to run $name", e)
+      case result: A => log.error(s"[Executor] $name: $result")
+    }
+  }
 
   def getVersionString[A: ClassTag]: String ={
     val cls = classTag[A].runtimeClass
@@ -18,18 +41,11 @@ object SparkDependencyHell extends App {
     s"$pkg $jar"
   }
 
-  def getVersionStrings: (String, String) = (getVersionString[DateTime], getVersionString[SpecificDatumReader[_]])
+  // Comparison of versions.
+  tryRunOnExecutor("Versions") { _ => (getVersionString[DateTime], getVersionString[SpecificDatumReader[_]]) }
 
-  val conf = new SparkConf()
-  val sc = new SparkContext(conf)
-
-  val log = LoggerFactory.getLogger(SparkDependencyHell.getClass)
-  log.error(s"Driver Version: $getVersionStrings")
-
-  val rdd = sc.parallelize(List(1))
-  val executorVersion = rdd.map(_ => getVersionStrings).collect.head
-
-  log.error(s"Executor Version: $executorVersion")
+  // Try to use joda-time method introduced in 2.0
+  tryRunOnExecutor("joda-time method introduced in 2.0") { _ => DateTime.now }
 
   sc.stop()
 }
